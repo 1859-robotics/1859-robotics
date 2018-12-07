a4b4086db6cb11ad899ec5b5c6c2758a95bace1c
alexa griffin <alexa.griffin42@gmail.com>
2018-12-06 21:42:57 -0500

    i am going to go to sleep now

--- content/blog/thoughts-on-turning-point.md
+++ content/blog/thoughts-on-turning-point.md
@@ -1,7 +1,12 @@
 ---
 title: Thoughts on turning point
 author: Alexa Griffin
-date: 6-12-2018
 ---
 
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis blanditiis, quas sed, nisi incidunt iste perferendis enim maiores ea unde dolore! Ad beatae qui illo praesentium a quod minima accusamus, iure eum ut iusto blanditiis quibusdam fugit doloremque dolor eius.
+So the new VRC game just got released, this one is insane, my initial thoughts are somewhat positive before learning that balls must be shot to toggle high flags I thought this game would be slightly too easy. I do not think this game is as good as starstruck was however I do think turning point is better than in the zone.
+
+First and foremost this game eliminates "skills bots," bots that have the pure intent of getting a high skills score and completely throw competition by the wayside. This is a much needed improvement from last year, because of the relative ease of creation of skills bots they were very prevalent this inherently is not a problem, if a team wants to focus on skills that's fine. However due to the collaborative nature of VRC the teams that focused on skills can and would hurt teams they were paired with in game.
+
+This game is also very multifaceted, you have to juggle stacking caps, shooting balls, and controlling platforms. This is a deparcher from past VRC games which have typically focused on one or maybe two tasks. I believe that this encourages specialization of bots thus lowering the barrier of entry while also challenging higher level teams to design around more things.
+
+Turning point is a very unorthodox game and I feel that it will provide some very interesting world's bots, but for now it is a lot of super sketchy bots that don't really do much, but isn't that how it is every year?
--- pages/blog/thoughts-on-turning-point.js
+++ pages/blog/thoughts-on-turning-point.js
@@ -15,7 +15,10 @@ export default () => (
         <h1>Thoughts on turning point</h1>
         <p>By: Alexa Griffin</p>
       </div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis blanditiis, quas sed, nisi incidunt iste perferendis enim maiores ea unde dolore! Ad beatae qui illo praesentium a quod minima accusamus, iure eum ut iusto blanditiis quibusdam fugit doloremque dolor eius.</p>
+      <p>So the new VRC game just got released, this one is insane, my initial thoughts are somewhat positive before learning that balls must be shot to toggle high flags I thought this game would be slightly too easy. I do not think this game is as good as starstruck was however I do think turning point is better than in the zone.</p>
+<p>First and foremost this game eliminates "skills bots," bots that have the pure intent of getting a high skills score and completely throw competition by the wayside. This is a much needed improvement from last year, because of the relative ease of creation of skills bots they were very prevalent this inherently is not a problem, if a team wants to focus on skills that's fine. However due to the collaborative nature of VRC the teams that focused on skills can and would hurt teams they were paired with in game.</p>
+<p>This game is also very multifaceted, you have to juggle stacking caps, shooting balls, and controlling platforms. This is a deparcher from past VRC games which have typically focused on one or maybe two tasks. I believe that this encourages specialization of bots thus lowering the barrier of entry while also challenging higher level teams to design around more things.</p>
+<p>Turning point is a very unorthodox game and I feel that it will provide some very interesting world's bots, but for now it is a lot of super sketchy bots that don't really do much, but isn't that how it is every year?</p>
     </div>
     <Footer />
   </div>
