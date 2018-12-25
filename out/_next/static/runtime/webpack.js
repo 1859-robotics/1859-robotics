c756381038967252b1a77f4ed9548ecf2c76db80
alexa griffin <alexa.griffin42@gmail.com>
2018-12-24 21:16:14 -0500

    heck

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6f1bbeac5987dd244c6e";
+/******/ 	var hotCurrentHash = "8371ad5344b0de2bdd95";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- out/_next/static/webpack/styles.6f1bbeac5987dd244c6e.hot-update.js
+++ out/_next/static/webpack/styles.6f1bbeac5987dd244c6e.hot-update.js
@@ -1,41 +1,4 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
-
-/***/ "./style/blog.sass":
-/*!*************************!*\
-  !*** ./style/blog.sass ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-// extracted by mini-css-extract-plugin;
-    if (true) {
-      var injectCss = function injectCss(prev, href) {
-        var link = prev.cloneNode();
-        link.href = href;
-        link.onload = function() {
-          prev.parentNode.removeChild(prev);
-        };
-        prev.stale = true;
-        prev.parentNode.insertBefore(link, prev);
-      };
-      module.hot.dispose(function() {
-        window.__webpack_reload_css__ = true;
-      });
-      if (window.__webpack_reload_css__) {
-        module.hot.__webpack_reload_css__ = false;
-        console.log("[HMR] Reloading stylesheets...");
-        var prefix = document.location.protocol + '//' + document.location.host;
-        document
-          .querySelectorAll("link[href][rel=stylesheet]")
-          .forEach(function(link) {
-            if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1545704098717");
-          });
-      }
-    }
-  
-
-/***/ }),
+webpackHotUpdate("styles",{
 
 /***/ "./style/footer.sass":
 /*!***************************!*\
@@ -103,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1545704099108");
+            injectCss(link, link.href.split("?")[0] + "?unix=1545704092613");
           });
       }
     }
@@ -148,5 +111,5 @@
 
 /***/ })
 
-}]);
-//# sourceMappingURL=styles.js.map
+})
+//# sourceMappingURL=styles.6f1bbeac5987dd244c6e.hot-update.js.map
